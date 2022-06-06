240c8ea3eb0a9ce07b58b1c704db592e3760188e
Martin <mkmartinoes@gmail.com>
2022-06-06 20:03:10 +0300

    admin login

--- control_flow.rb
+++ control_flow.rb
@@ -1,9 +1,19 @@
 def admin_login(username, password)
   # your code here
+  if (username == 'admin' || username == 'ADMNIN') && password == "12345"
+    return "Access granted"
+  else 
+    return "Access denied"
+  end
 end
+
+# puts admin_login("admin", '12345')
 
 def hows_the_weather(temperature)
   # your code here
+  response = nil
+  if temperature < 40
+    response = 'brisk'
 end
 
 def fizzbuzz(num)
